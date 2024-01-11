0d42f20e21bc664245db28eda929db445cde5850
Thiago Fernando <thiago.fernando@msn.com>
2024-01-11 11:32:54 -0300

    inital commit

---
+++ OpenWhatsapp/OpenWhatsapp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenWhatsapp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ OpenWhatsapp/OpenWhatsapp/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace OpenWhatsapp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+        async void OpenWhatsapp(object sender, EventArgs e)
+        {
+            await Browser.OpenAsync($"https://wa.me/55{numeroTelefone.Text}");
+        }
+    }
+
+}
